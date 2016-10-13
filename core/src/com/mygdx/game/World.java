3179da89a43589f0ae9541177883f6196d91dc44
BB <BB@10.2.55.238>
2016-10-13 14:15:04 +0700

    คลาส World

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -4,9 +4,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 
 public class World {
-	
-	private GameScreen gameScreen;
-	
-	
+	private Pacman pacman;
+    private PacmanGame pacmanGame;
+ 
+    World(PacmanGame pacmanGame) {
+        this.pacmanGame = pacmanGame;
+ 
+        pacman = new Pacman(100,100);
+    }
+ 
+    Pacman getPacman() {
+        return pacman;
+    }
 	
 }
