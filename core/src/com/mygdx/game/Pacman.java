c3d68f3c2b6c5b03d849dcabbf632e8413beca73
BB <BB@10.2.55.238>
2016-10-14 11:06:26 +0700

    วิ่งไม่ทะลุกำแพง

--- core/src/com/mygdx/game/Pacman.java
+++ core/src/com/mygdx/game/Pacman.java
@@ -6,16 +6,16 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Pacman {
 	private Vector2 position;
-	
 	private int currentDirection;
     private int nextDirection;
+	private Maze maze;
 	public static final int SPEED = 5;
 	public static final int DIRECTION_UP = 1;
 	public static final int DIRECTION_RIGHT = 2;
 	public static final int DIRECTION_DOWN = 3;
 	public static final int DIRECTION_LEFT = 4;
 	public static final int DIRECTION_STILL = 0;
-	private static final int [][] DIR_OFFSETS = new int [][] {
+    private static final int [][] DIR_OFFSETS = new int [][] {
         {0,0},
         {0,-1},
         {1,0},
@@ -23,10 +23,11 @@ public class Pacman {
         {-1,0}
     };
 	
-	public Pacman(int x,int y) {
+	public Pacman(int x,int y, Maze maze) {
 		position = new Vector2(x,y);
 		currentDirection = DIRECTION_STILL;
         nextDirection = DIRECTION_STILL;
+        this.maze = maze;
 	}
 	
 	public Vector2 getPosition() {
@@ -38,13 +39,17 @@ public class Pacman {
     }
 	
 	public void update() {
-        if(isAtCenter()) {
-            currentDirection = nextDirection;
-        }
+		 if(isAtCenter()) {
+	            if(canMoveInDirection(nextDirection)) {
+	                currentDirection = nextDirection;    
+	            } else {
+	                currentDirection = DIRECTION_STILL;    
+	            }
+	        }
         position.x += SPEED * DIR_OFFSETS[currentDirection][0];
         position.y += SPEED * DIR_OFFSETS[currentDirection][1];
     }
-	
+
 	public boolean isAtCenter() {
         int blockSize = WorldRenderer.BLOCK_SIZE;
  
@@ -52,8 +57,18 @@ public class Pacman {
                 ((((int)position.y - blockSize/2) % blockSize) == 0);
     }
 	
-	/*public void move(int dir) {
-		position.x += SPEED * DIR_OFFSETS[dir][0];
-		position.y += SPEED * DIR_OFFSETS[dir][1];
-	}*/
+	private int getRow() {
+        return ((int)position.y) / WorldRenderer.BLOCK_SIZE; 
+    }
+ 
+    private int getColumn() {
+        return ((int)position.x) / WorldRenderer.BLOCK_SIZE; 
+    }
+    
+    private boolean canMoveInDirection(int dir) {
+        int newRow = getRow() + DIR_OFFSETS[dir][1];
+        int newCol = getColumn() + DIR_OFFSETS[dir][0];
+ 
+        return !(maze.hasWallAt(newRow, newCol));
+    }
 }
